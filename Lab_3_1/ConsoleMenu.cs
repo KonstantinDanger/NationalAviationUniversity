bbf8d9ea47c768eeaed33d71f655cbdba434c8dd
DangerKostya <kostyan4i4ik2003@gmail.com>
2022-09-21 01:28:09 +0300

    Lab 3.1

--- Lab_3_1/ConsoleMenu.cs
+++ Lab_3_1/ConsoleMenu.cs
@@ -1,4 +1,5 @@
-﻿namespace FileManagement;
+﻿using System.Text.RegularExpressions;
+namespace FileManagement;
 
 public class ConsoleMenu
 {
@@ -7,13 +8,24 @@ public class ConsoleMenu
         University university = new University();
         FileHandler fileHandler = new FileHandler();
         Dentistry dentistry = new Dentistry();
+        Storyteller[] storytellers = new Storyteller[0]; 
         bool showSpecial = false;
 
-        //add student Michael Burry true 12:12:2001:2001 24Kf3sd 4 207743 84
-        //add student Gregory Ramsey true 9:12:2000:2000 24Kf3sd 5 78673 78
-        //add student Anny May false 12:12:2000:2001 DLB3gf 5 43523 92
+        string[] dateTimeFormats = {"M/d/yyyy h:mm:ss tt", "M/d/yyyy h:mm tt",
+                                           "MM/dd/yyyy hh:mm:ss", "M/d/yyyy h:mm:ss",
+                                           "M/d/yyyy hh:mm tt", "M/d/yyyy hh tt",
+                                           "M/d/yyyy h:mm", "M/d/yyyy h:mm",
+                                           "MM/dd/yyyy hh:mm", "M/dd/yyyy hh:mm",
+                                           "MM/d/yyyy HH:mm:ss.ffffff" };
+
+        //add student Michael Burry true 12/12/2001 24Kf3sd 4 207743 84
+        //add student Gregory Ramsey true 9/12/2000 24Kf3sd 5 78673 98
+        //add student Anny May false 12/12/2000 DLB3gf#(%^@^#@($3454798 5 43523 92
+        //add student Lilith May false 12/12/2000 DLB3gf 5 43523 56
 
         //add dentist Gregory Burry true 3 5000
+
+        //add storyteller Mark Pattison true Word,AnotherWord,ThirdWord,SomeWord
 
         //@"C:\Users\SerGun\Desktop\ООП\Лаб№3.1\Lab_3_1\IOData\Data.txt"
         //@"C:\Users\SerGun\Desktop\ООП\Лаб№3.1\Lab_3_1\IOData\Output.txt"
@@ -25,47 +37,50 @@ public class ConsoleMenu
             Console.WriteLine("------------------------------------------------------------------");
             Console.WriteLine("To add a student enter \"add student\" then enter: \"firstName\" \"lastName\" \"isMale\" \"birthDate\" \"studentID\" \"cource\" \"identity code\" \"average mark\"");
             Console.WriteLine("To add a dentist enter \"add dentist\" then enter: \"firstName\" \"lastName\" \"isMale\" \"qualification level\" \"salary\"");
+            Console.WriteLine("To add a storyteller enter \"add storyteller\" then enter: \"firstName\" \"lastName\" \"isMale\" \"vocabulary \"word,word,word...\" \"");
             Console.WriteLine("*!File path shouldn't contain empty entries!*");
             Console.WriteLine("To read information from file enter \"read \"filePath\"\"");
             Console.WriteLine("To write information to file enter \"write \"filePath\"\"");
             Console.WriteLine("To show all excellent female students on 5th cource enter \"show \"excellent\"\"");
             Console.WriteLine("To make student study enter \"student\" \"student index\" \".study\"");
             Console.WriteLine("To make dentist study enter \"dentist\" \"dentist index\" \".study\"");
+            Console.WriteLine("To make storyteller study enter \"storyteller\" \"storyteller index\" \".study\"");
             Console.WriteLine("To clear read text from the buffer enter \"clear buffer\"");
             Console.WriteLine("To end the program enter \"exit\"");
             Console.WriteLine("------------------------------------------------------------------");
             Console.ForegroundColor = ConsoleColor.Blue;
             #endregion
 
-            #region output
+            #region output  
 
             if(university.Length != 0)
             {
-                Console.WriteLine("\n------------------------------------------------------------------");
                 if (showSpecial)
                 {
+                    Console.WriteLine("\n------------------------------------------------------------------");
                     Console.WriteLine("Excellent female students on 5th course:\n");
-                }
-                else
-                {
-                    Console.WriteLine("Student list: ");
-                }
 
-                for (int i = 0; i < university.Length; i++)
-                {
-                    if (showSpecial)
+                    for (int i = 0; i < university.Length; i++)
                     {
                         if (!university[i].IsMale && university[i].IsExcellent() && university[i].Course == 5)
+                        {
                             Console.WriteLine($"{university[i]}\n");
+                        }
                     }
-                    else
-                    {
-                        Console.WriteLine($"\n{i + 1}) {university[i]}");
-                    }
+                }
+
+                Console.WriteLine("------------------------------------------------------------------");
+                Console.WriteLine("Student list: ");
+               
+                for (int i = 0; i < university.Length; i++)
+                {
+                    Console.WriteLine($"\n{i + 1}) {university[i]}");
                 }
             }
 
-            if(dentistry.Length != 0)
+            showSpecial = false;
+
+            if (dentistry.Length != 0)
             {
                 Console.WriteLine("\n------------------------------------------------------------------");
                 Console.WriteLine("Dentist list: ");
@@ -76,8 +91,18 @@ public class ConsoleMenu
                 }
             }
 
+            if (storytellers.Length != 0)
+            {
+                Console.WriteLine("\n------------------------------------------------------------------");
+                Console.WriteLine("Storytellers list: ");
+
+                for (int i = 0; i < storytellers.Length; i++)
+                {
+                    Console.WriteLine($"\n{i + 1}) {storytellers[i]}");
+                }
+            }
+
             Console.ForegroundColor = ConsoleColor.Green;
-            showSpecial = false;
             #endregion
 
             Console.WriteLine("\n------------------------------------------------------------------");
@@ -93,36 +118,31 @@ public class ConsoleMenu
                     {
                         if (str.Length != 10)
                         {
-                            ErrorLog("Student array lenght error!");
+                            ErrorLog("Student array length error!");
                             continue;
                         }
 
-                        string firstName = str[2];
-                        string lastName = str[3];
-                        bool.TryParse(str[4], out bool isMale);
-                        DateTime.TryParse(str[5], out DateTime birthDate);
-                        string studentID = str[6];
-                        int.TryParse(str[7], out int cource);
-                        int.TryParse(str[8], out int identityCode);
-                        int.TryParse(str[9], out int mark);
-
-                        university.Add(new(firstName, lastName, isMale, birthDate, studentID, cource, identityCode, mark));
+                        AddStudent(str, university);
                     }
                     else if (input.Contains("dentist"))
                     {
                         if (str.Length != 7)
                         {
-                            ErrorLog("Dentist array lenght error!");
+                            ErrorLog("Dentist array length error!");
                             continue;
                         }
 
-                        string firstName = str[2];
-                        string lastName = str[3];
-                        bool.TryParse(str[4], out bool isMale);
-                        int.TryParse(str[5], out int qualificationLevel);
-                        int.TryParse(str[6], out int salary);
+                        AddDentist(str, dentistry);
+                    }
+                    else if (input.Contains("storyteller"))
+                    {
+                        if (str.Length != 6)
+                        {
+                            ErrorLog("Storyteller array length error!");
+                            continue;
+                        }
 
-                        dentistry.Add(new(firstName, lastName, isMale, qualificationLevel, salary));
+                        AddStoryteller(str, ref storytellers);
                     }
                 }
                 else if (input.Contains("list"))
@@ -143,6 +163,12 @@ public class ConsoleMenu
                 }
                 else if (input.Contains("show"))
                 {
+                    if (university.Length == 0)
+                    {
+                        ErrorLog("There are no students yet!");
+                        continue;
+                    }
+
                     showSpecial = true;
                 }
                 else if (input.Contains("exit"))
@@ -180,6 +206,16 @@ public class ConsoleMenu
 
                         dentistry[index - 1].Study();
                     }
+                    else if (input.Contains("storyteller"))
+                    {
+                        if (index == 0)
+                        {
+                            ErrorLog("There are no storytellers yet!");
+                            continue;
+                        }
+
+                        storytellers[index - 1].Study();
+                    }
 
                 }
                 else if (input == "")
@@ -209,6 +245,51 @@ public class ConsoleMenu
         Console.ReadLine();
     }
 
+    private static void AddStudent(string[] str, University university)
+    {
+        string namePattern = @"[^a-zA-Z]+";
+        string numberPattern = @"[^0-9]+";
+
+        string firstName = Regex.Replace(str[2], namePattern, "");
+        string lastName = Regex.Replace(str[3], namePattern, "");
+        bool.TryParse(str[4], out bool isMale);
+        DateTime.TryParse(Regex.Replace(str[5], @"[^0-9:/]+", ""), out DateTime birthDate);
+        string studentID = Regex.Replace(str[6], @"[^a-zA-Z0-9]+", "");
+        int.TryParse(Regex.Replace(str[7], numberPattern, ""), out int cource);
+        int.TryParse(Regex.Replace(str[8], numberPattern, ""), out int identityCode);
+        int.TryParse(Regex.Replace(str[9], numberPattern, ""), out int mark);
+
+        university.Add(new(firstName, lastName, isMale, birthDate, studentID, cource, identityCode, mark));
+    }
+
+    private static void AddDentist(string[] str, Dentistry dentistry)
+    {
+        string namePattern = @"[^a-zA-Z]+";
+        string numberPattern = @"[^0-9]+";
+
+        string firstName = Regex.Replace(str[2], namePattern, "");
+        string lastName = Regex.Replace(str[3], namePattern, "");
+        bool.TryParse(str[4], out bool isMale);
+        int.TryParse(Regex.Replace(str[5], numberPattern, ""), out int qualificationLevel);
+        int.TryParse(Regex.Replace(str[6], numberPattern, ""), out int salary);
+
+        dentistry.Add(new(firstName, lastName, isMale, qualificationLevel, salary));
+    }
+
+    private static void AddStoryteller(string[] str, ref Storyteller[] storytellers)
+    {
+        string namePattern = @"[^a-zA-Z]+";
+        string numberPattern = @"[^0-9]+";
+
+        string firstName = Regex.Replace(str[2], namePattern, "");
+        string lastName = Regex.Replace(str[3], namePattern, "");
+        bool.TryParse(str[4], out bool isMale);
+        string vocabulary = Regex.Replace(String.Join(", ", str[5].Split(',', StringSplitOptions.None)), @"[^a-zA-Z', ]+", "");
+
+        Array.Resize(ref storytellers, storytellers.Length + 1);
+        storytellers[storytellers.Length - 1] = new (firstName, lastName, isMale, vocabulary);
+    }
+
     private static void ErrorLog(string message = "There's no such command!")
     {
         Console.Clear();
@@ -223,4 +304,3 @@ public class ConsoleMenu
         Console.SetWindowSize(width, height);
     }
 }
-
--- Lab_3_1/Storyteller.cs
+++ Lab_3_1/Storyteller.cs
@@ -1,16 +1,39 @@
-﻿public class Storyteller : Person, ICook
+﻿public class Storyteller : Person, ICook, IStudy
 {
-    private enum StoryType { fantasy, drama, comedy, horror }
-    private StoryType storyType;
+    public string Vocabulary { get; private set; }
+
+    public Storyteller() : base()
+    {
+        Vocabulary = default;
+    }
+
+    public Storyteller(string firstName, string lastName, bool isMale, string vocabulary) : base(firstName, lastName, isMale)
+    {
+        Vocabulary = vocabulary;
+    }
 
     public void Cook()
     {
         Console.WriteLine("Cook veal");
     }
 
+    public void Study()
+    {
+        string letters = "abcdefghijklmnopqrstuvwxyz";
+        int count = new Random().Next(5, 15);
+        string word = "";
+
+        for (int i = 0; i < count; i++)
+        {
+            word += letters[new Random().Next(0, letters.Length-1)];
+        }
+
+        Vocabulary += $", {word}";
+    }
+
     public override string ToString()
     {
-        return base.ToString() + new String($"\nstory type: {storyType.ToString()}");
+        return base.ToString() + new String($"\nVocabulary: {Vocabulary}");
     }
 }
 
